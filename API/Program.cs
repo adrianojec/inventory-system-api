a8b0227c330498391ef2a1c241964b03bc6bef0f
adrianojec <adrianojeric.28@gmail.com>
2023-03-27 22:11:46 +0800

    added security definition

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Extensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+   opt.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+   opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+   {
+      Name = "Authorization",
+      Type = SecuritySchemeType.ApiKey,
+      Scheme = "Bearer",
+      BearerFormat = "JWT",
+      In = ParameterLocation.Header,
+      Description = "JWT Authorization header using the Bearer scheme."
+   });
+   opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+        new string[] {}
+        }
+    });
+});
 
 // Database Connection
 builder.Services.AddDbContext<DataContext>(opt =>
